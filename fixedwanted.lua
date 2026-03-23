f5e03e693cb9845c0f13fb6cebab11ea551c6278
dyletafk <galagatilivna@gmail.com>
2026-03-23 19:21:28 +0200

    Update fixedwanted.lua

--- fixedwanted.lua
+++ fixedwanted.lua
@@ -11,8 +11,8 @@ local u8 = encoding.UTF8
 --  НАСТРОЙКИ
 -- ============================================================
 local RECORDINGS_FOLDER = "D:\\Records"
-local LOG_FILE          = "D:\\Games\Arizona Games Launcher\\bin\rodina\\moonloader\\wanted_log.html"
-local BAT_FILE          = "D:\\Games\Arizona Games Launcher\\bin\rodina\\moonloader\\open_log.bat"
+local LOG_FILE          = "D:\\Games\\Arizona Games Launcher\\bin\rodina\\moonloader\\wanted_log.html"
+local BAT_FILE          = "D:\\Games\\Arizona Games Launcher\\bin\rodina\\moonloader\\open_log.bat"
 -- ============================================================
 
 local show_window = imgui.ImBool(false)
